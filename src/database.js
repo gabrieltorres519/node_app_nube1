6f153c9ff3cb5d7cddba34038b69b22a1be2a8e3
unknown <gabrieltorres9909@gmail.com>
2022-09-01 13:42:33 -0500

    Configuración básica de conexión al servicio mongo en el docker compose

--- src/database.js
+++ src/database.js
@@ -1,1 +1,5 @@
 //Aquí va la cadena de conexión de Mongo
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/mibase')
+    .then(db => console.log("Base de datos conectada", db.connection.host))
+    .catch(err=>console.err(err))
