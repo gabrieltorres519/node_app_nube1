638a80533a4d81943f449bb9f35347e3d137f8ef
unknown <gabrieltorres9909@gmail.com>
2022-09-01 14:06:59 -0500

    Configuración correcta para crear el contenedor de la app y usar docker-compose build docker-compose up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('./database');
 //Por qué puerto escuchar a un puerto
 app.listen(3000);
 console.log('Servidor activo en puerto',3000);
